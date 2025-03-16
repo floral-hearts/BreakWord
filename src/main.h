46303b0bab07f2e2f2e472dac4f8063327b3130a
floral-hearts <hermit183@outlook.com>
2025-03-16 14:40:06 +0900

    ファイル挿入

--- src/main.h
+++ src/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#ifdef __linux__
+#include <termios.h>
+#elif _WIN32 || _WIN64
+#include <conio.h>
+#endif
+
 int main(void);
 int key(void);
 void changeKeyMode(void);
