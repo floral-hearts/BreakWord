6ec4960aec31bbbe693c47eeb6bbb118db47e25f
floral-hearts <hermit183@outlook.com>
2025-03-17 14:26:17 +0900

    Create word-std3.h

---
+++ src/word-std3.h
@@ -0,0 +1,29 @@
+#ifndef WORD__STD3
+#define WORD__STD3
+
+JChar std3[][] = {
+    {A , _ , SU},
+    {A , _ , TI},
+    {A , _ , TO},
+    {A , _ , RU},
+    {A , I , GI},
+    {A , I , KO},
+    {A , I , GO},
+    {A , I , SU},
+    {A , I , ZU},
+    {A , I , SO},
+    {A , I , DA},
+    {A , I , TE},
+    {A , I , MA},
+    {A , U , TO},
+    {A , U , N },
+    {A , E , N },
+    {A , O , I },
+    {A , O , KO},
+    {A , O , TA},
+    {A , O , NA},
+    {A , O , BA},
+    {A , O , MI},
+}
+
+#endif
