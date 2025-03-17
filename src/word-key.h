2b100ea7213b4ac485e4b244e6ccdad25ba5dcda
floral-hearts <hermit183@outlook.com>
2025-03-17 14:53:10 +0900

    Create word-key.h

---
+++ src/word-key.h
@@ -0,0 +1,24 @@
+#ifndef WORD__KEY_H
+#define WORD__KEY_H
+
+JChar key[][] = {
+{A, A, I, E, BA, KO, U, I, U},
+{A, _, RU, NO, NA, I, TU, KI, NO, KA, KI, HA, YO, KU, NA, I},
+{A, I, E, N, KI, E, N},
+{A, I, O, O, KE, RE, BA, NI, KU, SI, MI, I, TA, RU},
+{A, I, KU, TI, NI, TU, BA},
+{A, I, GO, SE, I, MO, KU},
+{A, I, SA, TU, HA, TO, KI, NO, U, ZI, GA, MI},
+{A, I, SA, TU, YO, RI, E, N, SA, TU},
+{A, I, SO, ZU, KA, SI, MO, KA, NE, KA, RA, O, KI, RU},
+{A, I, SO, MO, KO, SO, MO, TU, KI, HA, TE, RU},
+{A, I, TA, I, NO, KO, TO, HA, KO, TI, XYA, SI, RA, NU},
+{A, I, TA, KU, TI, GA, FU, SA, GA, RA, NA, I},
+{A, I, TA, KU, TI, NI, HA, TO, HA, TA, TA, NU},
+{A, I, TA, KU, TI, HE, BO, TA, MO, TI},
+{A, I, DU, TI, WO, U, TU},
+{A, I, TE, KA, WA, RE, DO, NU, SI, KA, WA, RA, ZU},
+{A, I, TE, NO, SA, SU, RU, KO, U, MI, XYO, U},
+}
+
+#endif
