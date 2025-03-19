073dbdaa8b4f644b85a641a107f8ef060110998e
floral-hearts <hermit183@outlook.com>
2025-03-20 07:49:47 +0900

    グローバル変数の共有宣言

--- src/set.h
+++ src/set.h
@@ -1,4 +1,15 @@
+/*
+ *  ゲームの設定の値を保存します。
+ */
+
 #ifndef SRC_SET_H
 #define SRC_SET_H
+
+extern int v_char_board;   // CharBoardの垂直方向の長さ
+extern int h_char_board;   // CharBoardの水平方向の長さ
+extern int v_gap;          // 文字の垂直方向の間隔の長さ（全角スペースの量）
+extern int h_gap;          // 文字の水平方向の間隔の長さ（改行の量）
+extern int min_word_size;  // 最小の単語の文字数
+extern int max_word_size;  // 最大の単語の文字数
 
 #endif
