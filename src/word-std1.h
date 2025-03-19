e7b0900c062af61ff998239b5bdb9f88d92743fb
floral-hearts <hermit183@outlook.com>
2025-03-20 07:31:48 +0900

    Delete src directory

--- src/word-std1.h
+++
@@ -1,8 +0,0 @@
-#ifndef WORD__STD1_H
-#define WORD__STD1_H
-
-const JChar std1[][1] = {
-    {I  }, {U  }, {E  }, {O  }, {KA }, {GA }, {KI }, {GI }, {KU }, {GU }, {KE }, {KO }, {GO }, {ZI }, {SU }, {ZU }, {SE }, {TA }, {TI }, {TU }, {TE }, {TO }, {NA }, {NI }, {NE }, {NO }, {HA }, {BA }, {HI }, {HU }, {BU }, {HE }, {HO }, {MA }, {MI }, {ME }, {MO }, {YA }, {YU }, {YO }, {RO }, {WA }
-};
-
-#endif
