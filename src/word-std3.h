1f0810d53c04714e31a3a29c6527b1169547bed9
floral-hearts <hermit183@outlook.com>
2025-03-19 12:52:31 +0900

    Update word-std3.h

--- src/word-std3.h
+++ src/word-std3.h
@@ -2,28 +2,7 @@
 #define WORD__STD3
 
 JChar std3[][] = {
-    {A , _ , SU},
-    {A , _ , TI},
-    {A , _ , TO},
-    {A , _ , RU},
-    {A , I , GI},
-    {A , I , KO},
-    {A , I , GO},
-    {A , I , SU},
-    {A , I , ZU},
-    {A , I , SO},
-    {A , I , DA},
-    {A , I , TE},
-    {A , I , MA},
-    {A , U , TO},
-    {A , U , N },
-    {A , E , N },
-    {A , O , I },
-    {A , O , KO},
-    {A , O , TA},
-    {A , O , NA},
-    {A , O , BA},
-    {A , O , MI},
+    {A  , I  , E  }, {A  , I  , KA }, {A  , I  , KI }, {A  , I  , GI }, {A  , I  , KO }, {A  , I  , GO }, {A  , I  , SA }, {A  , I  , ZA }, {A  , I  , SI}, {A  , I  , ZA}
 }
 
 #endif
