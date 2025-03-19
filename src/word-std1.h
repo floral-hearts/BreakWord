b7680b0cfa59d4db503e285933a0d9df6caedaf7
floral-hearts <hermit183@outlook.com>
2025-03-19 11:30:31 +0900

    Update word-std1.h

--- src/word-std1.h
+++ src/word-std1.h
@@ -1,49 +1,8 @@
 #ifndef WORD__STD1_H
 #define WORD__STD1_H
 
-JChar std1[1][] = {
-    {I },
-    {U },
-    {E },
-    {O },
-    {KA},
-    {GA},
-    {KI},
-    {GI},
-    {KU},
-    {GU},
-    {KE},
-    {KO},
-    {GO},
-    {ZI},
-    {SU},
-    {ZU},
-    {SE},
-    {TA},
-    {TI},
-    {TU},
-    {TE},
-    {TO},
-    {NA},
-    {NI},
-    {NE},
-    {NO},
-    {HA},
-    {BA},
-    {HI},
-    {HU},
-    {BU},
-    {HE},
-    {HO},
-    {MA},
-    {MI},
-    {ME},
-    {MO},
-    {YA},
-    {YU},
-    {YO},
-    {RO},
-    {WA},
+const JChar std1[][1] = {
+    {I  }, {U  }, {E  }, {O  }, {KA }, {GA }, {KI }, {GI }, {KU }, {GU }, {KE }, {KO }, {GO }, {ZI }, {SU }, {ZU }, {SE }, {TA }, {TI }, {TU }, {TE }, {TO }, {NA }, {NI }, {NE }, {NO }, {HA }, {BA }, {HI }, {HU }, {BU }, {HE }, {HO }, {MA }, {MI }, {ME }, {MO }, {YA }, {YU }, {YO }, {RO }, {WA }
 };
 
 #endif
