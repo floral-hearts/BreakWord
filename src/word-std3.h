fd07425c0cd0edf0efd64a0feba64dc3ae2ff32c
floral-hearts <hermit183@outlook.com>
2025-03-19 14:56:40 +0900

    Update word-std3.h

--- src/word-std3.h
+++ src/word-std3.h
@@ -1,8 +1,10 @@
 #ifndef WORD__STD3
 #define WORD__STD3
 
-JChar std3[][] = {
-    {A  , I  , E  }, {A  , I  , KA }, {A  , I  , KI }, {A  , I  , GI }, {A  , I  , KO }, {A  , I  , GO }, {A  , I  , SA }, {A  , I  , ZA }, {A  , I  , SI}, {A  , I  , ZA}
-}
+JChar std3[][3] = {
+    {A  , I  , E  }, {A  , I  , KA }, {A  , I  , KI }, {A  , I  , GI }, {A  , I  , KO }, {A  , I  , GO }, {A  , I  , SA }, {A  , I  , ZA }, {A  , I  , SI }, {A  , I  , ZA }, {A  , I  , SI }, {A  , I  , ZI }, {A  , I  , SU }, {A  , I  , ZU }, {A  , I  , SO}, {A  , I  , DA }, {A  , I  , TU }, {A  , I  , TE }, {A  , I  , BA }, {A  , I  , BI}, {A  , I  , FU }, {A  , I  , BU }, {A  , I  , BO }, {A  , I  , MA }, {A  , I  , YA }, {A  , I  , RU }, {A  , I  , RO }, {A  , I  , WA },
+    {A  , U  , TO }, {A  , U  , N  },
+    {A  , E  , GI }, {A  , E  , RA }, {A  , E  , N  },
+};
 
 #endif
