a34227010dbb1d76c8f34a92c58f284e742bac42
floral-hearts <hermit183@outlook.com>
2025-03-20 09:24:12 +0900

    putjc関数を制作

--- src/j-char.c
+++ src/j-char.c
@@ -1,1 +1,11 @@
 #include "j-char.h"
+
+void putjc(J_Char jc) {
+    char js[4];
+    js[0] = 0xe3;
+    js[1] = jc < JC_MU ? 0x81 : 0x82;
+    js[2] = jc < JC_MU ? jc + 0x81 : (jc - JC_MU) + 0x81;
+    js[3] = 0x00;
+    printf("%s", js);
+    return;
+}
